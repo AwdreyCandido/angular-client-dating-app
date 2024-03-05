c61025ffeddcc0fa8ead2612afce63c366c82152
AwdreyCandido <awdrey.dev@gmail.com>
2024-03-05 00:57:24 -0300

    end of section 5 - client login and register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  baseUrl = 'https://localhost:5001/api';
   title = 'Hello, world!!!';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe({
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  getUsers() {
+    this.http.get(`${this.baseUrl}/users`).subscribe({
       next: (response) => {
         this.users = response;
       },
@@ -22,8 +33,14 @@ export class AppComponent implements OnInit {
       },
       complete: () => {
         console.log('Request has completed');
-        console.log(this.users)
       },
     });
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  registerMode = false;
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
+}
